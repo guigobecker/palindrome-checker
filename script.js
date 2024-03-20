8ab21e9cf5e77f94d531da373b444bff03611d17
Guigo <rsbecker@inf.ufsm.br>
2024-03-20 19:36:12 -0300

    Estilizar animações do alerta

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const entrada = document.getElementById('entrada');
+const alerta = document.getElementById('alerta');
+
+function mostrarAlerta(bool){
+    if(bool) {
+        alerta.classList.add('sim');
+        alerta.classList.remove('escondido');
+        alerta.classList.add('visivel');
+
+    } else {
+        alerta.classList.add('nao');
+        alerta.classList.remove('escondido');
+        alerta.classList.add('visivel');
+    }
+
+    setTimeout(function() {
+        alerta.classList.add('escondido');
+        alerta.classList.remove('visivel');
+    }, 3000)
+}
 
 function inverterString(str) {
     return str.split('').reverse().join('');
@@ -8,7 +27,7 @@ function verificarString() {
     const str = entrada.value;
     const inverso = inverterString(str);
     
-    str === inverso ? alert('É um palíndromo!') : alert('Não é um palíndromo.');
+    str === inverso ? mostrarAlerta(true) : mostrarAlerta(false);
 
     entrada.value = '';
 }
