ae4b3307dbcd5fc6993111bac8249530346c4431
Guigo <rsbecker@inf.ufsm.br>
2024-03-20 21:45:22 -0300

    Corrigir comportamento do alerta

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const mensagem = document.getElementById('mensagem');
 
 function mostrarAlerta(bool){
     if(bool) {
-        mensagem.innerText = 'Sua palavra é um palíndromo!';
+        mensagem.innerText = 'É um palíndromo!';
         alerta.classList.add('sim');
         alerta.classList.remove('escondido');
         alerta.classList.add('visivel');
 
     } else {
-        mensagem.innerText = 'Sua palavra não é um palíndromo.';
+        mensagem.innerText = 'Não é um palíndromo.';
         alerta.classList.add('nao');
         alerta.classList.remove('escondido');
         alerta.classList.add('visivel');
@@ -19,6 +19,8 @@ function mostrarAlerta(bool){
     setTimeout(function() {
         alerta.classList.add('escondido');
         alerta.classList.remove('visivel');
+        alerta.classList.remove('sim');
+        alerta.classList.remove('nao');
         mensagem.innerText = ''
     }, 3000)
 }
